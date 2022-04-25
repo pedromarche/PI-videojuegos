c02b4078d8110da60744fc9b85c6823511cc139b
pedro marchetto <you@example.com>
2022-04-25 01:34:20 -0300

    100 videojuegos llamados

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -29,7 +29,7 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  img: {
+  background_image: {
       type: DataTypes.STRING,
     }
 
--- api/src/routes/modules/videogames.js
+++ api/src/routes/modules/videogames.js
@@ -5,21 +5,32 @@ const axios = require('axios');
 const router = Router();
 const {Videogame, Genre } = require('../../db');
 
-const getFromApi = async() => { //traigo los juegos de la api
-    const gameList = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`);
-    const props = await gameList.data.results.map(e => {
-        return {
-           name : e.name,
-           background_image : e.background_image,
-           genres : e.genres.map(e => e.name),
-        }
-    });
+const getFromApi = async() => { //traigo los 100 juegos de la api
+    const gameList1 = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`);
+    const gameList2 = await axios.get(gameList1.data.next);
+    const gameList3 = await axios.get(gameList2.data.next);
+    const gameList4 = await axios.get(gameList3.data.next);
+    const gameList5 = await axios.get(gameList4.data.next);
+    const gameList = gameList1.data.results
+                    .concat(gameList2.data.results)
+                    .concat(gameList3.data.results)
+                    .concat(gameList4.data.results)
+                    .concat(gameList5.data.results) 
+    
+    const props = gameList.map(e => {
+                    return {
+                        name : e.name,
+                        background_image : e.background_image,
+                         genres : e.genres.map(e => e.name),
+                         }
+                    });
+    
     return props;
+    
 }
 
-const getFromDb = async() => {
+const getFromDb = async() => { //traigo juegos de la bd
     return await Videogame.findAll({
-        
         include: {
             model: Genre,
             attributes: ['name'],
@@ -30,7 +41,7 @@ const getFromDb = async() => {
     })
 }
 
-const allVideogames = async() => {
+const allVideogames = async() => { // armo una lista con todos los videojuegos
     const api = await getFromApi();
     const db = await getFromDb();
     const allApiDb = api.concat(db);
@@ -60,11 +71,11 @@ router.get('/', async(req, res) => {
 
 router.post('/', async(req, res) => {
     try{
-        const {name, description, platforms, released, rating, image, genres} = req.body
+        const {name, description, platforms, released, rating, background_image, genres} = req.body
         if(!name || !description || !platforms){
             return res.status(404).json('Falta completar datos obligatorios')
         }
-        const haveGame = await Videogame.create({name, description, platforms, released, rating, image});
+        const haveGame = await Videogame.create({name, description, platforms, released, rating, background_image});
         const haveGenre = await Genre.findAll({
             where: { name: genres }
         })
