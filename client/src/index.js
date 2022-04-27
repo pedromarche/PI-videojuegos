67a7703706498454cd9c92e7b60ebfcbd2ff9184
pedro marchetto <you@example.com>
2022-04-27 01:17:20 -0300

    empezando el front

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.render(
+  <provider> store = {store}
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </provider>,
   document.getElementById('root')
 );
 
